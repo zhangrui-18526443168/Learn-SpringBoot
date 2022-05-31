7d9ac6498c18c1b9ed0092486173b9d6371728d0
zhangrui_glb <tute170231@163.com>
2022-05-31 15:53:49 +0800

    增加注解@MockBean

--- api/src/test/java/com/mengyunzhi/springBootStudy/controller/KlassControllerTest.java
+++ api/src/test/java/com/mengyunzhi/springBootStudy/controller/KlassControllerTest.java
@@ -1,10 +1,12 @@
 package com.mengyunzhi.springBootStudy.controller;
 
+import com.mengyunzhi.springBootStudy.repository.KlassRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -21,6 +23,9 @@ public class KlassControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    KlassRepository klassRepository;
 
     @Test
     public void save() throws Exception {
